a9804c8798629c915fbabe8b07783ab9bbc09d16
gustavoo151 <josegustavooliveira121@gmail.com>
2023-11-07 13:50:01 -0300

    Agora está finalizado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,36 +19,38 @@ public class Main {
                 String resposta = "";  // variável para armazenar a resposta da requisição do cliente
                 String path = getRequestPath(reader.readLine()); // obtém o caminho da requisição do cliente
 
-                if (path.equals("/endpoint1")) {
+                if (path.equals("/endpoint1")) {  // verifica se o caminho é o endpoint1, retorna 200
                     resposta = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" +
                             "<html><title>Endpoint 1 </title><body><h1>Endpoint 1 - Para http 200</h1></body></html>";
 
-                } else if (path.equals("/endpoint2")) {
+                } else if (path.equals("/endpoint2")) { // verifica se o caminho é o endpoint2, retorna 404
                     resposta = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" +
                             "<html><title>Endpoint 2</title><body><h1>Endpoint 2 - Para http 404</h1></body></html>";
-                } else if (path.equals("/endpoint3")) {
+
+                } else if (path.equals("/endpoint3")) { // verifica se o caminho é o endpoint3, retorna 500
                     resposta = "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/html\r\n\r\n" +
                             "<html><title>Endpoint 3</title><body><h1>Endpoint 3 - Para http 500</h1></body></html>";
-                } else {
+
+                } else { // caso o caminho não seja nenhum dos endpoints, retorna 404
                     resposta = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" +
                             "<html><title>Not Found</title><body><h1>404 - Not Found</h1></body></html>";
                 }
 
-                conexao.getOutputStream().write(resposta.getBytes());
-                conexao.close();
+                conexao.getOutputStream().write(resposta.getBytes());  // Aqui envia a resposta para o cliente, convertendo a string em bytes, que é o que o cliente espera receber.
+                conexao.close();  //  fecha a conexão com o cliente
             }
-        } catch (IOException e) {
+        } catch (IOException e) { // trata exceções de entrada e saída
             e.printStackTrace();
         }
     }
 
-    private static String getRequestPath(String requestLine) {
+    private static String getRequestPath(String requestLine) { // Esse método pega a linha de requisição do cliente e retorna o caminho da requisição
         if (requestLine != null) {
             String[] parts = requestLine.split(" ");
             if (parts.length >= 2) {
-                return parts[1];
+                return parts[1];  // retorna o caminho da requisição.  Ex: /endpoint1
             }
         }
-        return "/";
+        return "/";  // retorna a raiz, caso não encontre o caminho da requisição.
     }
 }
