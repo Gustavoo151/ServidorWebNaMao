c116954122f2bc4cf2b3957e989ed449c397406d
gustavoo151 <josegustavooliveira121@gmail.com>
2023-11-07 09:58:30 -0300

    Terminei eu acho

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,66 @@
 package org.example;
 
+import javax.swing.*;
+import java.io.*;
+import java.net.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            ServerSocket servidor = new ServerSocket(2222);  // cria um socket de servidor na porta 2222
 
-        System.out.println("Teste");
+            while (true) {  // loop infinito para aguardar conexões
+                Socket conexao = servidor.accept();  // aguarda conexão com o cliente
+
+                InputStream is = conexao.getInputStream(); // obtém o fluxo de entrada da conexão
+                is = new BufferedInputStream(is);  // cria um buffer para o fluxo de entrada
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is)); // cria um leitor de fluxo de entrada
+//                String s; // variável para armazenar as linhas lidas do fluxo de entrada
+//
+//                while((s = reader.readLine()) != null) {
+//                    System.out.println(s);
+//                    if (s.trim().isEmpty()) {  // se a linha lida for vazia, então encerra o loop
+//                        break;
+//                    }
+//                }
+
+                String resposta = "";  // variável para armazenar a resposta da requisição do cliente
+                String path = getRequestPath(reader.readLine()); // obtém o caminho da requisição do cliente
+
+//                System.out.println("Mostrando: " + reader.readLine());
+
+
+                if (path.equals("/endpoint1")) {
+                    resposta = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" +
+                            "<html><title>Endpoint 1</title><body><h1>Endpoint 1</h1></body></html>";
+
+                } else if (path.equals("/endpoint2")) {
+                    resposta = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" +
+                            "<html><title>Endpoint 2</title><body><h1>Endpoint 2</h1></body></html>";
+                } else if (path.equals("/endpoint3")) {
+                    resposta = "HTTP/1.1 500 Internal Server Error\r\nContent-Type: text/html\r\n\r\n" +
+                            "<html><title>Endpoint 3</title><body><h1>Endpoint 3</h1></body></html>";
+                } else {
+                    resposta = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n" +
+                            "<html><title>Not Found</title><body><h1>404 - Not Found</h1></body></html>";
+                }
+
+                conexao.getOutputStream().write(resposta.getBytes());
+                conexao.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getRequestPath(String requestLine) {
+        if (requestLine != null) {
+            String[] parts = requestLine.split(" ");
+            if (parts.length >= 2) {
+                return parts[1];
+            }
+        }
+        return "/";
     }
 }
